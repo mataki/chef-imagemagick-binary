59f76b9825370f48d794830e3dfc70325e25cf66
mat_aki <mat_aki@sonicgarden.jp>
2022-06-24 09:55:42 +0900

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-imagemagick-binary
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef-imagemagick-binary
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
