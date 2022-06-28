f18913bf88e947974f2563e49b035f0a0e685b27
mat_aki <mat_aki@sonicgarden.jp>
2022-06-28 10:29:24 +0900

    Removed unnecessary hierarchy of settings

--- recipes/download.rb
+++ recipes/download.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-remote_file node['chef-imagemagick-binary']['source']['file_path'] do
-  source node['chef-imagemagick-binary']['source']['url']
+remote_file node['chef-imagemagick-binary']['file_path'] do
+  source node['chef-imagemagick-binary']['url']
   mode '755'
 
   action :create_if_missing
--- recipes/policy.rb
+++ recipes/policy.rb
@@ -1,17 +1,17 @@
-directory node['chef-imagemagick-binary']['source']['policy_directory'] do
+directory node['chef-imagemagick-binary']['policy_directory'] do
   mode '0755'
-  owner node['chef-imagemagick-binary']['source']['policy_user']
-  group node['chef-imagemagick-binary']['source']['policy_group']
+  owner node['chef-imagemagick-binary']['policy_user']
+  group node['chef-imagemagick-binary']['policy_group']
   recursive true
 
   action :create
 end
 
-file node['chef-imagemagick-binary']['source']['policy_path'] do
+file node['chef-imagemagick-binary']['policy_path'] do
   content '<policymap><policy domain="delegate" rights="none" pattern="*" /><policy domain="filter" rights="none" pattern="*" /><policy domain="coder" rights="none" pattern="*" /><policy domain="coder" rights="read|write" pattern="{PNG,JPEG,JPG,GIF,WEBP}" /></policymap>'
   mode '0755'
-  owner node['chef-imagemagick-binary']['source']['policy_user']
-  group node['chef-imagemagick-binary']['source']['policy_group']
+  owner node['chef-imagemagick-binary']['policy_user']
+  group node['chef-imagemagick-binary']['policy_group']
 
   action :create_if_missing
 end
